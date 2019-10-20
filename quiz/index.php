5d4df14978446c0d8499b3c35b188a77217d0ce0
Riki Martua Aritonang <rikimartua20@gmail.com>
2019-10-20 15:57:23 +0700

    Add files via upload

---
+++ quiz/index.php
@@ -0,0 +1,268 @@
+			<!DOCTYPE html>
+			<html>
+			<head>
+				<meta http-equiv="cotent-type" content="text/html">
+				<meta name="author" content="DMET School">
+			<title>RIKI MARTUA</title>
+			<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+			<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+			<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+			<link href='https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css' rel='stylesheet' type='text/css'/>
+			<link rel="stylesheet" href="asset/css/styl.css">
+			<link rel="stylesheet" type="text/css" href="asset/source/jquery.fancybox.js?v=2.1.5" media="screen">
+		</head>
+		<body>
+			<!--Navbar -->
+			<nav class="navbar navbar-default navbar-static-top">
+				<div class="container" id="navbar">
+					<div class="navbar-header">
+						<a class="navbar-brand navbar-link" ><h2></h2></a>
+						<button class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navcol-1">
+							<span class="sr-only"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+					</div>
+					<div class="collapse navbar-collapse" id="navcol-1">
+						<ul class="nav navbar-nav navbar-right">
+							<li ><a href="#home">HOME</a></li>
+							<li ><a href="#Activity">ACTIVITY</a></li>
+							<li ><a href="#education">EDUCATION</a></li>
+							<li ><a href="#gallery">GALLERY</a></li>
+							<li ><a href="#about">ABOUT</a></li>
+
+						</div>
+					</nav>
+					<!--/.Navbar -->
+					<!-- DESC -->
+					<div id="background">
+						<div class="jumbotron">
+							<p><b>HALLO !!!</b> selamat datang di webside pertama yang ku buat,disini kalian bisa mengenali ku lebih jauh dengan membaca seluruh bagian di halaman ini , karena pepatah mengatakan "tak kenal maka tak tau,tak tau maka tak kenal",<br>Salam kenal...!</p>
+							<h1>Martua</h1>
+							
+						</div>
+					</div>
+
+					<!-- DECS -->
+					<!-- CONTAINER ATAS-->
+					<div id="home">
+						<div class="isi">
+							<div class="container atas">
+								<h1>WHO AM I ?</h1>
+								<div class="row">
+									<div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+										<i class="glyphicon glyphicon-star"></i>
+
+										<h3>FAVORIT</h3>
+										<p class="text-justify">
+											Berkata tantang favorit saya memiliki club sepakbola yang sedari dulu ku favoritkan untuk setiap kompetisi yaitu Barcelona Fc yang orang lainpun pasti mengenalnya yang terkenal dengan tikitaka gameplay
+										</div>
+										<div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+											<i class="glyphicon glyphicon-user"></i>
+											<h3>PROFIL</h3>
+											<p class="text-justify">
+												<br>Kenalin Namaku Riki martua aritonang asal daerah jambi, tepatnya masih di Negara Indonesia,aku anak bungsu dari 4 bersaudara, sekarang aku sekolah di UPN 'Veteran' Yogyakarta tepatnya pada jurusan Informatika tahun 2018
+											</p>
+										</div>
+										<div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+											<i class="glyphicon glyphicon-knight"></i>
+											<h3>HOBBY</h3>
+											<p class="text-justify">
+												Hobby tak jauh dari kesukaan, hobby ku yaitu bermain game, Game Pes 2010 game yang sepakola yang pertama ku mainkan, yang sedang tenarnya pada saat itu, but PUBG is number 1 fav game.
+											</p>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+
+
+						<!-- CONTAINER ATAS-->
+						<!-- Container bawah-->
+
+						<div id="Activity">
+							<div class="container bawah">
+								<h1>My Activity</h1>
+								<div class="col-lg-4 col-md-4 col-sm-12 col-xs-12"><a href="asset/img/act1.jpg" class="perbesar" rel="gallery"><img class="img-circle" src="asset/img/act1.jpg" width="220px"></a>
+									<a href="https://www.instagram.com/itcupnyk/"><h3>ITC</h3></a>
+									<p class="text-canter text-justify"><strong>ITC </strong>atau Information Technology Club yang bertempat di UPN 'Veteran' Yogyakarta, ITC memiliki 3 bidang yaitu Android,web dan HR,sesi ini adalah sesi foto pengurus ITC 2019 </p>
+								</div>
+								<div class="col-lg-4 col-md-4 col-sm-12 col-xs-12"><a href="asset/img/act2.jpeg" class="perbesar" rel="gallery"><img class="img-circle" src="asset/img/act2.jpeg" width="200px"></a>
+									<a href="https://www.instagram.com/ukmkristen/"><h3>YBC</h3></a>
+									<p class="text-canter text-justify"><strong>YBC </strong>ini adalah salah satu kegiatan yang diselenggarakan oleh UKM Kristen UPN 'Veteran' Yogyakarta yang pasti acaranya seru menarik dan penuh kasih,ikutan sini .</p>
+								</div>
+								<div  class="col-lg-4 col-md-4 col-sm-12 col-xs-12" ><a href="asset/img/act3.JPG" class="perbesar" rel="gallery"><img class="img-circle" src="asset/img/act3.JPG" width="250px" ></a>
+									<a href="https://www.instagram.com/ukmkristen/"><h3>OUTBOND YBC</h3></a>
+									<p class="text-canter text-justify"><strong>OUTBOND YBC </strong>ini juga salah satu kegiatan yang diselenggarakan oleh UKM Kristen UPN 'Veteran' Yogyakarta yaitu pada acara YBC,ikutan tahun depan </p>
+								</div>
+
+							</div>
+						</div>
+						<!-- Container bawah-->
+
+						<!-- education-->
+						<div id="education">
+							<div class="container educ">
+								<h2>My Education</h2>
+								<div class="row">
+									<div class="col-lg-3 col-md-3 col-sm-6 col-xs-6">
+										<div class="thumbnail">
+											<img src="asset/img/sd.jpg">
+											<div class="caption">
+												<h3>SDN 98/x R.Indah</h3>
+												<p class="text-justify">SDN 98/X Rantau indah tepatnya di kabupaten tanjung jabung timur, provinsi jambi.</p><br><br>
+												
+											</div>
+										</div>
+
+									</div>
+									<div class="col-lg-3 col-md-3 col-sm-6 col-xs-6">
+										<div class="thumbnail">
+											<img src="asset/img/smp.png">
+											<div class="caption">
+												<h3>SMPN 4 TJT</h3>
+												<p class="text-justify">SMPN 4 Tanjung Jabung Timur  tepatnya di kabupaten tanjung jabung timur, provinsi jambi</p><br><br>
+												
+											</div>
+										</div>
+
+									</div>
+									<div class="col-lg-3 col-md-3 col-sm-6 col-xs-6">
+										<div class="thumbnail">
+											<img class="sma" src="asset/img/sma.jpg">
+											<div class="caption">
+												<h3>SMPN 4 TJT</h3>
+												<p class="text-justify">
+													<p class="text-canter text-justify"> SMAN 4 Tanjung Jabung Timur tepatnya di kabupaten tanjung jabung timur, provinsi jambi</p>
+													<a href="https://www.facebook.com/sman4tanjabtim/"><button class="btn btn-dark" type="button">Read More</button></a>
+												</div>
+											</div>
+
+										</div>
+										<div class="col-lg-3 col-md-3 col-sm-6 col-xs-6">
+											<div class="thumbnail">
+												<img src="asset/img/kuliah.png">
+												<div class="caption">
+													<p class="text-justify"><h3>UPNVYK</h3></a>
+														<p class="text-canter text-justify">UPN 'Veteran' Yogyakarta tepatnya Kec. Depok, Kabupaten Sleman, Daerah Istimewa Yogyakarta
+															<a href="http://www.upnyk.ac.id/"><button  class="btn btn-outline-dark" type="button">Read More</button></a>
+														</div>
+
+													</div>
+
+												</div>
+											</div>
+										</div>
+										<!-- education-->
+
+
+										<!-- Gallery-->
+										<div id="gallery">
+											<div class="container">
+												<h1>Gallery</h1>
+												<div class="row">
+													<div class="col-lg-3 col-md-3 col-sm-3 col-xs-3" id="satu"><a href="asset/img/gallery4.JPG" class="perbesar" rel="gallery"><img class="img-responsive" src="asset/img/gallery4.JPG" width="300px"></a>
+
+													</div>
+													<div class="col-lg-3 col-md-3 col-sm-3 col-xs-3" id="satu"><a href="asset/img/gallery2.JPG" class="perbesar" rel="gallery"><img class="img-responsive" src="asset/img/gallery2.JPG" width="300px"></a>
+
+													</div>
+													<div class="col-lg-3 col-md-3 col-sm-3 col-xs-3" id="satu"><a href="asset/img/gallery1.JPG" class="perbesar" rel="gallery"><img class="img-responsive" src="asset/img/gallery1.JPG" width="300px"></a>
+
+													</div>
+													<div class="col-lg-3 col-md-3 col-sm-3 col-xs-3" id="dua"><a href="asset/img/act3.JPG" class="perbesar" rel="gallery"><img class="img-responsive" src="asset/img/act3.JPG" width="300px"></a>
+
+													</div>
+
+
+												</div>
+
+											</div>
+
+
+
+											<!-- gallery -->
+											<!-- about -->
+											<div id="about">
+												<div class="isi">
+													<div class="container footer">
+
+														<div class="col-lg-3 col-md-3 col-sm-6 col-xs-6" >
+															<img src="asset/img/about.jpg" height="200px">
+															<div class="icon">
+																<a href="https://www.facebook.com/riki.junior.505"><i  class="fa fa-facebook"></i></a>
+																<a href="hhttps://www.youtube.com/channel/UC-5o7euWXU5rpAH_aEY_1HA?view_as=subscriber"><i class="fa fa-youtube-play"></i></a>
+																<a href="https://www.instagram.com/rikiaritonang12/"><i class="fa fa-instagram"></i></a>
+															</div>
+
+
+														</div>
+														<div class="gambar">
+															<p>"Dont compare yourself with himself<br>but be better to improve your self"<br><br>~ Riki Martua Aritonang~</p>
+
+														</div>
+
+
+
+													</div>
+												</div>
+											</div>
+
+
+
+											<!-- about -->
+											<!-- Footer -->
+
+											<div id="footer">
+												<div class="container">
+													<h5> Copyright Martua 2019</h5>
+												</div>
+
+											</div>
+
+
+											<!-- Foorter -->
+
+
+
+
+											<script type="asset/js/jquery.min.js"></script>
+											<script type="asset/bootstrap/js/bootstrap.min.js"></script>
+										
+											<script type="text/javascript" src="asset/lib/jquery-1.10.1.min.js"></script>
+											<script type="text/javascript" src="asset/source/jquery.fancybox.js?v=2.1.5"></script>
+											<script type="text/javascript">
+												$(document).ready(function(){
+													$(".perbesar").fancyboy();
+												});
+											</script>
+											<script>
+												$('a[href*="#"]')
+											  .not('[href="#"]')
+											  .not('[href="#0"]')
+											  .click(function(event) {
+											    // On-page links
+											    if (
+											      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+											      && 
+											      location.hostname == this.hostname
+											    ) {
+											      // Figure out element to scroll to
+											      var target = $(this.hash);
+											      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+											      // Does a scroll target exist?
+											      if (target.length) {
+											        // Only prevent default if animation is actually gonna happen
+											        event.preventDefault();
+											        $('html, body').animate({
+											          scrollTop: target.offset().top
+											        }, 1000);
+											        return false;
+											      }
+											    }
+											  });
+											</script>
+
+</body>
+</html>
